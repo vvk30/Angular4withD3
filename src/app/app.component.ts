3aa9ef98541933f97cc659e14cd4e08c50c49409
Vishal Kulkarni <vishal.kulkarni303@gmail.com>
2017-11-14 15:23:26 +0530

    Added bar chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   private chartData: Array<any>;
 
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
     // give everything a chance to get loaded before starting the animation to reduce choppiness
@@ -23,10 +23,8 @@ export class AppComponent implements OnInit {
   generateData() {
     this.chartData = [];
     for (let i = 0; i < (8 + Math.floor(Math.random() * 10)); i++) {
-      this.chartData.push([
-        `Index ${i}`,
-        Math.floor(Math.random() * 100)
-      ]);
+      this.chartData.push(
+        Math.floor(Math.random() * 100));
     }
   }
 }
